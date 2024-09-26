49cdc0748b8edef08597473ac97adc3a04b6ade4
Vkev <huynhkhang7452@gmail.com>
2024-09-26 21:51:02 +0700

    Add ResponseList

--- TicketResell.Services/Services/ResponseModel.cs
+++ TicketResell.Services/Services/ResponseModel.cs
@@ -7,52 +7,57 @@ namespace TicketResell.Services.Services
 {
     public class ResponseModel
     {
+        public string ServiceName { get; set; } = null!;
+        public string FunctionName { get; set; } = null!;
         public int StatusCode { get; set; }
         public string Status { get; set; } = null!;
         public string Message { get; set; } = null!;
         public object? Data { get; set; }
 
+
         public ResponseModel()
         {
         }
 
-        private ResponseModel(int statusCode, string status, string message, object? data)
+        private ResponseModel(int statusCode, string status, string message, object? data, string serviceName, string functionName)
         {
+            ServiceName = serviceName;
+            FunctionName = functionName;
             StatusCode = statusCode;
             Status = status;
             Message = message;
             Data = data;
         }
 
-        public static ResponseModel Success(string message, object? data)
+        public static ResponseModel Success(string message, object? data, string serviceName, string functionName)
         {
-            return new ResponseModel(200, "Success", message, data);
+            return new ResponseModel(200, "Success", message, data, serviceName, functionName);
         }
-        public static ResponseModel Success(string message)
+        public static ResponseModel Success(string message, string serviceName, string functionName)
         {
-            return new ResponseModel(200, "Success", message, null);
-        }
-
-
-        public static ResponseModel Error(string message)
-        {
-            return new ResponseModel(500, "Error", message, null);
-        }
-
-        public static ResponseModel NotFound(string message)
-        {
-            return new ResponseModel(404, "Not Found", message, null);
+            return new ResponseModel(200, "Success", message, null, serviceName, functionName);
         }
 
 
-        public static ResponseModel BadRequest(string message)
+        public static ResponseModel Error(string message, string serviceName, string functionName)
         {
-            return new ResponseModel(400, "Bad Request", message, null);
+            return new ResponseModel(500, "Error", message, null, serviceName, functionName);
         }
 
-        public static ResponseModel BadRequest(string message, object? data)
+        public static ResponseModel NotFound(string message, string serviceName, string functionName)
         {
-            return new ResponseModel(400, "Bad Request", message, data);
+            return new ResponseModel(404, "Not Found", message, null, serviceName, functionName);
+        }
+
+
+        public static ResponseModel BadRequest(string message, string serviceName, string functionName)
+        {
+            return new ResponseModel(400, "Bad Request", message, null, serviceName, functionName);
+        }
+
+        public static ResponseModel BadRequest(string message, object? data, string serviceName, string functionName)
+        {
+            return new ResponseModel(400, "Bad Request", message, data, serviceName, functionName);
         }
     }
 }
