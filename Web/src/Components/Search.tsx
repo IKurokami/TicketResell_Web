2fa1cf321390a0e50c2e24524ea6d7cf3f8ac4d3
Gwang <114713193+gwang08@users.noreply.github.com>
2024-09-30 11:08:56 +0700

    test

---
+++ Web/src/Components/Search.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React, { useState } from 'react';
+import { FaSearch, FaChevronDown, FaChevronUp } from 'react-icons/fa';
+import '@/Css/Search.css';
+
+const SearchPage: React.FC = () => {
+return(
+    <p>LE mINH qUANG</p>
+    );
+};
+
+export default SearchPage;
