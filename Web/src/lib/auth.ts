ec9a283e59c329b0031029ba29a877ff37a5c3d3
Vinh <0001.kurokami@gmail.com>
2024-10-14 11:13:23 +0700

    updat package

--- Web/src/lib/auth.ts
+++ Web/src/lib/auth.ts
@@ -1,1 +1,13 @@
-    
+import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: "",
+      clientSecret: "",
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
